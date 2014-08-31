bde3c93efeee4628e221ed6bdf24eda5e45e0d93
Jamie English <jamienglish@gmail.com>
2014-08-31 14:08:06 +0100

    Tidy up, add logging, print output to stdout

--- src/crawler/core.clj
+++ src/crawler/core.clj
@@ -3,72 +3,96 @@
             [clojure.xml :as xml]
             [clojure.string :as s]
             [clojure.data.json :as json]
-            [clojure.core.async :refer [chan <! >! >!! go go-loop alts! timeout thread buffer]])
-  (:import [org.jsoup Jsoup])
+            [clojure.core.async :refer [chan <! <!! >! >!! go go-loop alts! timeout buffer]])
+  (:import [org.jsoup Jsoup]
+           [java.net URL])
   (:gen-class))
 
 (def visited-urls (atom #{}))
 (def site-map (atom {}))
 (def urls-chan (chan 102400))
-(def exit-channel (chan 1))
+(def log-chan (chan))
+(def exit-chan (chan 1))
+(def concurrency 1000)
+
+;; Logging
+(defn log [& msgs]
+  (go (>! log-chan (s/join " " msgs))))
+
+(defn start-logger []
+  (go (while true
+        (let [msg (<! log-chan)]
+          (binding [*out* *err*]
+            (println msg))))))
+
+;; URL helpers
+(defn string->url [url]
+  (try
+    (URL. url)
+    (catch java.net.MalformedURLException e
+      (log "Couldn't parse url:" url)
+      nil)))
 
 (defn base-url [url-str]
-  (let [url (java.net.URL. url-str)]
+  (let [url (URL. url-str)]
     (str (.getProtocol url) "://" (.getHost url))))
 
-(defn get-doc [url]
-  (let [{:keys [body trace-redirects]} (http/get url)]
-    (Jsoup/parse body (base-url (last trace-redirects)))))
-
+;; Fetching/parsing pages
 (defn get-doc [url]
   (try
-    (-> (http/get url)
-        (:body)
-        (Jsoup/parse (base-url url)))
+    (let [{:keys [body trace-redirects]} (http/get url)]
+      (Jsoup/parse body (base-url (last trace-redirects))))
     (catch Exception e
-      (println "Exception fetching" url))))
+      (log "Exception fetching" url)
+      nil)))
 
 (defn get-assets [doc]
   (->> (.select doc "script, link, img, a")
-       (map #(or (.attr % "abs:href") (.attr % "abs:src")))
-       (remove nil?)
+       (mapcat #(conj [] (.attr % "abs:href") (.attr % "abs:src")))
+       (filter string?)
        (filter #(or (.endsWith % ".css") (.endsWith % ".js")))))
 
-(defn get-links [doc]
+(defn get-links [doc crawling-url]
   (->> (.select doc "a")
        (map #(.attr % "abs:href"))
-       (filter #(= "gocardless.com" (.getHost (java.net.URL. %))))
-       (filter #(#{"http" "https"} (.getProtocol (java.net.URL. %))))
-       (map #(s/replace (java.net.URL. %) #"\#.*" ""))))
-
-(defn without-protocol [uri]
-  (s/replace uri #"^(http|https)://" ""))
+       (remove empty?)
+       (map string->url)
+       (remove nil?)
+       (filter #(.endsWith (.getHost %) (.getHost (URL. crawling-url))))
+       (filter #(#{"http" "https"} (.getProtocol %)))
+       (map #(s/replace % #"\#.*" ""))))
 
 (defn process-page [url]
-  (when-not (@visited-urls (without-protocol url))
-    (swap! visited-urls conj (without-protocol url))
-    (println "crawling" url)
+  (when-not (@visited-urls url)
+    (swap! visited-urls conj url)
+    (log "crawling" url)
     (when-let [doc (get-doc url)]
       (swap! site-map assoc url (get-assets doc))
-      (doseq [url (get-links doc)]
+      (doseq [url (get-links doc url)]
         (go (>! urls-chan url))))))
 
-(defn main-loop []
-  (dotimes [_ 50]
+;; Main event loop
+(defn start-consumers []
+  (dotimes [_ concurrency]
     (go-loop [url (<! urls-chan)]
              (process-page url)
              (let [[value channel] (alts! [urls-chan (timeout 5000)])]
                (if (= channel urls-chan)
                  (recur value)
-                 (>! exit-channel true))))))
+                 (>! exit-chan true))))))
+
+(defn seconds-since [start-time]
+  (double (/ (- (System/currentTimeMillis) start-time) 1000)))
 
 (defn -main
   "Crawls [domain] for links to assets"
   [domain]
-  (println "Crawling" domain)
-  (main-loop)
-  (go (>! urls-chan "http://gocardless.com"))
-  (go (<! exit-channel)
-      (println "COMPLETED")
-      (spit "output.json" (json/write-str @site-map))
-      (System/exit 0)))
+  (let [start-time (System/currentTimeMillis)]
+    (start-logger)
+    (log "Begining crawl of" domain)
+    (start-consumers)
+    (>!! urls-chan domain)
+    (<!! exit-chan)
+    (println (json/write-str @site-map))
+    (<!! (log "Completed after" (seconds-since start-time) "seconds"))
+    (System/exit 0)))
